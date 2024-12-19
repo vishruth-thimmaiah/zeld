c76aadbb3566d61bc559380d04b6dc2a3d44f2b8
Vishruth-Thimmaiah <vishruththimmaiah@gmail.com>
2024-12-19 22:21:27 +0530

    feat: get endian type from header

--- src/parser/header.zig
+++ src/parser/header.zig
@@ -31,25 +31,34 @@ pub const ElfHeader = struct {
             std.debug.print("Error: File is not an ELF file\n", .{});
         }
 
+        const endian = getEndianness(magic);
+
         return ElfHeader{
             .class = magic[4],
             .data = magic[5],
             .version = magic[6],
             .osabi = magic[7],
             .abiversion = magic[8],
-            .type = std.mem.readInt(u16, bytes[16..18], std.builtin.Endian.little),
-            .machine = std.mem.readInt(u16, bytes[18..20], std.builtin.Endian.little),
-            .file_version = std.mem.readInt(u32, bytes[20..24], std.builtin.Endian.little),
-            .entry = std.mem.readInt(u64, bytes[24..32], std.builtin.Endian.little),
-            .phoff = std.mem.readInt(u64, bytes[32..40], std.builtin.Endian.little),
-            .shoff = std.mem.readInt(u64, bytes[40..48], std.builtin.Endian.little),
-            .flags = std.mem.readInt(u32, bytes[48..52], std.builtin.Endian.little),
-            .ehsize = std.mem.readInt(u16, bytes[52..54], std.builtin.Endian.little),
-            .phentsize = std.mem.readInt(u16, bytes[54..56], std.builtin.Endian.little),
-            .phnum = std.mem.readInt(u16, bytes[56..58], std.builtin.Endian.little),
-            .shentsize = std.mem.readInt(u16, bytes[58..60], std.builtin.Endian.little),
-            .shnum = std.mem.readInt(u16, bytes[60..62], std.builtin.Endian.little),
-            .shstrndx = std.mem.readInt(u16, bytes[62..64], std.builtin.Endian.little),
+            .type = std.mem.readInt(u16, bytes[16..18], endian),
+            .machine = std.mem.readInt(u16, bytes[18..20], endian),
+            .file_version = std.mem.readInt(u32, bytes[20..24], endian),
+            .entry = std.mem.readInt(u64, bytes[24..32], endian),
+            .phoff = std.mem.readInt(u64, bytes[32..40], endian),
+            .shoff = std.mem.readInt(u64, bytes[40..48], endian),
+            .flags = std.mem.readInt(u32, bytes[48..52], endian),
+            .ehsize = std.mem.readInt(u16, bytes[52..54], endian),
+            .phentsize = std.mem.readInt(u16, bytes[54..56], endian),
+            .phnum = std.mem.readInt(u16, bytes[56..58], endian),
+            .shentsize = std.mem.readInt(u16, bytes[58..60], endian),
+            .shnum = std.mem.readInt(u16, bytes[60..62], endian),
+            .shstrndx = std.mem.readInt(u16, bytes[62..64], endian),
         };
+    }
+    fn getEndianness(magic: [16]u8) std.builtin.Endian {
+        switch (magic[5]) {
+            0x01 => return std.builtin.Endian.little,
+            0x02 => return std.builtin.Endian.big,
+            else => unreachable,
+        }
     }
 };
