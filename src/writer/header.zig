36e10a8b51aedb9a921d344d8559c5898e13e3bc
Vishruth-Thimmaiah <vishruththimmaiah@gmail.com>
2025-01-31 23:33:08 +0530

    feat: writer for elf header

---
+++ src/writer/header.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const parser = @import("parser");
+
+pub fn writeHeader(header: parser.ElfHeader) ![64]u8 {
+    var bytes: [64]u8 = undefined;
+    bytes[0..4].* = parser.MAGIC_BYTES;
+    bytes[4] = header.class;
+    bytes[5] = if (header.data == std.builtin.Endian.little) 1 else 2;
+    bytes[6] = header.version;
+    bytes[7] = header.osabi;
+    bytes[8] = header.abiversion;
+
+    bytes[16..18].* = std.mem.toBytes(header.type);
+    bytes[18..20].* = std.mem.toBytes(header.machine);
+    bytes[20..24].* = std.mem.toBytes(header.file_version);
+    bytes[24..32].* = std.mem.toBytes(header.entry);
+    bytes[32..40].* = std.mem.toBytes(header.phoff);
+    bytes[40..48].* = std.mem.toBytes(header.shoff);
+    bytes[48..52].* = std.mem.toBytes(header.flags);
+    bytes[52..54].* = std.mem.toBytes(header.ehsize);
+    bytes[54..56].* = std.mem.toBytes(header.phentsize);
+    bytes[56..58].* = std.mem.toBytes(header.phnum);
+    bytes[58..60].* = std.mem.toBytes(header.shentsize);
+    bytes[60..62].* = std.mem.toBytes(header.shnum);
+    bytes[62..64].* = std.mem.toBytes(header.shstrndx);
+
+    return bytes;
+}
--- src/writer/writer.zig
+++ src/writer/writer.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 const parser = @import("parser");
+const writeHeader = @import("header.zig").writeHeader;
 
 pub fn writer(elf: parser.Elf64, filename: []const u8) !void {
-    var file = try std.fs.cwd().createFile(filename, .{.mode = 0o777});
+    var file = try std.fs.cwd().createFile(filename, .{ .mode = 0o777 });
     defer file.close();
 
-    try file.writeAll(&parser.MAGIC_BYTES);
-    _ = elf;
+    try file.writeAll(&(try writeHeader(elf.header)));
 }
