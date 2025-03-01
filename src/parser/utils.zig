3ef5539e74afdaa6ecd12067209763ec6549d802
Vishruth-Thimmaiah <vishruththimmaiah@gmail.com>
2025-03-01 19:29:13 +0530

    feat: utils.readInt can now return enums

--- src/parser/utils.zig
+++ src/parser/utils.zig
@@ -1,14 +1,18 @@
 const std = @import("std");
 
 pub fn readInt(comptime T: type, bytes: []const u8, offset: usize, endian: std.builtin.Endian) T {
-    const size = switch (T) {
-        u8, i8 => 1,
-        u16, i16 => 2,
-        u32, i32 => 4,
-        u64, i64 => 8,
+    const size = @sizeOf(T);
+    switch (@typeInfo(T)) {
+        .Int => {
+            return std.mem.readInt(T, bytes[offset .. offset + size][0..size], endian);
+        },
+        .Enum => {
+            const int_type = std.meta.Int(.unsigned, @bitSizeOf(T));
+            const a = std.mem.readInt(int_type, bytes[offset .. offset + size][0..size], endian);
+            return @as(T, @enumFromInt(a));
+        },
         else => unreachable,
-    };
-    return std.mem.readInt(T, bytes[offset .. offset + size][0..size], endian);
+    }
 }
 
 pub fn getEndianness(magic: *const [16]u8) std.builtin.Endian {
