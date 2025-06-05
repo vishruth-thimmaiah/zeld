fa871eb30a18afd1a53e55502188fddbf30942cd
Vishruth-Thimmaiah <vishruththimmaiah@gmail.com>
2025-06-06 01:33:11 +0530

    feat: cleanup segment builder logic

--- src/linking/pheaders.zig
+++ src/linking/pheaders.zig
@@ -2,102 +2,90 @@ const std = @import("std");
 const elf = @import("elf");
 const ElfLinker = @import("linker.zig").ElfLinker;
 
-const SegmentArray = std.ArrayList(*elf.Section);
-const SegmentMap = std.AutoHashMap(struct { elf.PHType, u32 }, SegmentArray);
+const SegmentArray = std.ArrayList(Segment);
 
 const Segment = union(enum) {
-    SEGMENT_START: struct { elf.PHType, u32, u64 },
+    SEGMENT_START: struct { elf.PHType, u32, u64, u64 },
     SEGMENT_END,
     SECTION: *elf.Section,
 
-    fn mapSegment(segments: *SegmentMap, section: *elf.Section, type_: elf.PHType, flags: u32) !void {
-        const segment = try segments.getOrPutValue(.{ type_, flags }, SegmentArray.init(segments.allocator));
-        try segment.value_ptr.append(section);
-    }
-
-    fn mapToSequence(
-        segments: *const SegmentArray,
-        pheaders: *std.ArrayList(Segment),
-        counter: *u32,
-        type_: elf.PHType,
-        flags: u32,
-        align_: u64,
-    ) !void {
-        try pheaders.append(.{ .SEGMENT_START = .{ type_, flags, align_ } });
-        counter.* += 1;
-        for (segments.items) |section| try pheaders.append(.{ .SECTION = section });
-
-        try pheaders.append(.SEGMENT_END);
+    fn addSingleSegment(segments: *SegmentArray, section: *elf.Section, offset: u64, type_: elf.PHType, flags: u32, align_: u64) !void {
+        try segments.append(.{ .SEGMENT_START = .{ type_, flags, align_, offset } });
+        try segments.append(.{ .SECTION = section });
+        try segments.append(.SEGMENT_END);
     }
 };
 
 fn SegmentBuilder(linker: *ElfLinker) !struct { []Segment, u32 } {
     const sections = linker.mutElf.sections.items;
 
-    var segments = SegmentMap.init(linker.allocator);
-    defer segments.deinit();
+    var segments = SegmentArray.init(linker.allocator);
+    var other_segments = SegmentArray.init(linker.allocator);
+    defer other_segments.deinit();
 
-    defer {
-        var value_iter = segments.valueIterator();
-        while (value_iter.next()) |entry| {
-            entry.deinit();
-        }
-    }
+    var counter: u32 = 0;
+    var addr: u64 = elf.START_ADDR;
+    var offset: u64 = 0x40;
 
     for (sections) |*section| {
         if (std.mem.eql(u8, section.name, ".interp")) {
-            try Segment.mapSegment(&segments, section, .PT_INTERP, 0b100);
-        } else if (section.flags & 0b110 == 0b110) {
-            try Segment.mapSegment(&segments, section, .PT_LOAD, 0b101);
-        } else if (section.flags & 0b011 == 0b011) {
-            try Segment.mapSegment(&segments, section, .PT_LOAD, 0b110);
-        } else if (section.type == .SHT_NOTE) {
-            try Segment.mapSegment(&segments, section, .PT_NOTE, 0b100);
-        } else if (section.flags & 0b010 == 0b010) {
-            try Segment.mapSegment(&segments, section, .PT_LOAD, 0b100);
-        } else {
-            return error.UnmappedSection;
+            try segments.append(.{ .SEGMENT_START = .{ .PT_INTERP, 0b100, 0x1, offset } });
+            counter += 1;
+            try segments.append(.{ .SECTION = section });
+            try segments.append(.SEGMENT_END);
         }
+        var flags: ?u32 = null;
+        switch (section.flags) {
+            0b110 => flags = 0b101,
+            0b011 => flags = 0b110,
+            0b010 => flags = 0b100,
+            else => {},
+        }
+        if (flags) |flag| {
+            const last = segments.getLastOrNull() orelse last: {
+                counter += 1;
+                try segments.append(.{ .SEGMENT_START = .{ .PT_LOAD, flag, 0x1000, offset } });
+                break :last segments.getLast();
+            };
+            if (last == .SECTION and last.SECTION.flags != section.flags) {
+                counter += 1;
+                addr += 0x1000;
+                try segments.append(.SEGMENT_END);
+                try segments.append(.{ .SEGMENT_START = .{ .PT_LOAD, flag, 0x1000, offset } });
+            } else if (last == .SEGMENT_END) {
+                counter += 1;
+                addr += 0x1000;
+                try segments.append(.{ .SEGMENT_START = .{ .PT_LOAD, flag, 0x1000, offset } });
+            }
+            try segments.append(.{ .SECTION = section });
+        }
+
+        if (section.type == .SHT_NOTE) {
+            try Segment.addSingleSegment(&other_segments, section, offset, .PT_NOTE, 0b100, 0x8);
+            counter += 1;
+        }
+
+        section.addr = addr;
+        addr += section.data.len;
+        offset += section.data.len;
+    }
+    try segments.append(.SEGMENT_END);
+
+    try segments.appendSlice(other_segments.items);
+
+    for (sections) |*section| {
+        section.addr += 0x40 + (counter + 1) * @sizeOf(elf.ProgramHeader);
     }
 
-    var segment_format = std.ArrayList(Segment).init(linker.allocator);
-    defer segment_format.deinit();
-
-    // TODO: verify the sections are continuous
-    var segment_count: u32 = 0;
-
-    if (segments.get(.{ .PT_INTERP, 0b100 })) |*interp|
-        try Segment.mapToSequence(interp, &segment_format, &segment_count, .PT_INTERP, 0b100, 0x1);
-
-    if (segments.get(.{ .PT_LOAD, 0b100 })) |*load|
-        try Segment.mapToSequence(load, &segment_format, &segment_count, .PT_LOAD, 0b100, 0x1000);
-
-    if (segments.get(.{ .PT_NOTE, 0b100 })) |*note|
-        try Segment.mapToSequence(note, &segment_format, &segment_count, .PT_NOTE, 0b100, 0x8);
-
-    if (segments.get(.{ .PT_LOAD, 0b101 })) |*load|
-        try Segment.mapToSequence(load, &segment_format, &segment_count, .PT_LOAD, 0b101, 0x1000);
-
-    if (segments.get(.{ .PT_LOAD, 0b110 })) |*load|
-        try Segment.mapToSequence(load, &segment_format, &segment_count, .PT_LOAD, 0b110, 0x1000);
-
-    return .{ try segment_format.toOwnedSlice(), segment_count };
+    return .{ try segments.toOwnedSlice(), counter };
 }
 
 pub fn generatePheaders(linker: *ElfLinker) !void {
     const sb = try SegmentBuilder(linker);
     const segments = sb.@"0";
+
     const segment_count = sb.@"1" + 1; // +1 for the PHDR
     defer linker.allocator.free(segments);
-
-    const sections = linker.mutElf.sections.items;
-
-    var vaddr: u64 = (elf.START_ADDR | 64) + segment_count * @sizeOf(elf.ProgramHeader);
-
-    for (sections) |*section| {
-        section.addr = vaddr;
-        vaddr += section.data.len;
-    }
 
     linker.mutElf.pheaders = std.ArrayList(elf.ProgramHeader).init(linker.allocator);
     const pheaders = &linker.mutElf.pheaders.?;
@@ -116,7 +104,7 @@ pub fn generatePheaders(linker: *ElfLinker) !void {
                 try pheaders.append(elf.ProgramHeader{
                     .type = start.@"0",
                     .flags = start.@"1",
-                    .offset = 0,
+                    .offset = if (load_count == 0) 0 else start.@"3" + segment_count * @sizeOf(elf.ProgramHeader),
                     .vaddr = if (load_count == 0) elf.START_ADDR else 0,
                     .paddr = if (load_count == 0) elf.START_ADDR else 0,
                     .filesz = memsz,
@@ -132,12 +120,8 @@ pub fn generatePheaders(linker: *ElfLinker) !void {
             .SECTION => |section| {
                 memsz += section.data.len;
                 const pheader = &pheaders.items[pheaders.items.len - 1];
-                if (pheader.type == .PT_LOAD) {
-                    section.addr += (load_count - 1) * 0x1000;
-                }
                 if (pheader.vaddr == 0) {
                     pheader.setAddr(section.addr);
-                    pheader.offset = section.addr - elf.START_ADDR - (load_count - 1) * 0x1000; // FIXME: Temporary hack
                 }
             },
         }
